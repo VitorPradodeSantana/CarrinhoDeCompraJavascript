c34e8954f488eb1e5d8b4f415c5232e128b16937
VitorPradodeSantana <vitor_shotokan@hotmail.com>
2022-09-19 21:59:13 -0300

    Avancando com o javascript, adicionando evento de click e tentando implementar o metodo de salvar no localstorage

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
 const produtos = [
   {
-    id: 1,
+    id: 0,
     descricao: 'camiseta balmain',
     preco: 50.0,
-    tamanho: 'p'
+    tamanho: 'p',
+    quantidade: 0
+  },
+  {
+    id: 1,
+    descricao: 'camiseta nike',
+    preco: 60.0,
+    tamanho: 'm',
+    quantidade: 0
   },
   {
     id: 2,
-    descricao: 'camiseta nike',
-    preco: 60.0,
-    tamanho: 'm'
-  },
-  {
-    id: 3,
     descricao: 'camiseta adidas',
     preco: 80.0,
-    tamanho: 'g'
+    tamanho: 'g',
+    quantidade: 0
   }
 ]
+
+let carrinhoDeCompra = []
 
 const listaProdutos = document.getElementById('listaProdutos')
 
 const carregarProdutos = () => {
   produtos.map(produto => {
     listaProdutos.innerHTML += `
+    
+      <div id="item">
 
-      <div>
-
-        <span>${produto.id}</span>
-        <p>${produto.descricao}</p>
-        <p>${produto.tamanho}
-        <p>${produto.preco}</p>
-        <a href="#">COMPRAR</a>
+        <span id="id">${produto.id}</span>
+        <p id="descricao">${produto.descricao}</p>
+        <p id="tamanho">${produto.tamanho}
+        <p id="preco">${produto.preco}</p>
+        <a href="#" class="adicionar" key=${produto.id}>adicionar</a>
 
       </div>
 
@@ -40,3 +45,52 @@ const carregarProdutos = () => {
 }
 
 carregarProdutos()
+
+const btnAdicionar = document.getElementsByClassName('adicionar')
+
+let pegarCarrinho = () => {
+  return localStorage.getItem('Carrinho')
+    ? JSON.parse(localStorage.getItem('Carrinho'))
+    : []
+}
+
+let hasItem = item => {
+  let carrinho = pegarCarrinho()
+  if (!item.idProduto) {
+    return false
+  }
+  return carrinho.some(itemCarrinho => item.idProduto == itemCarrinho.id)
+}
+
+let salvarProdutoCarrinho = item => {
+  let carrinho = pegarCarrinho()
+  if (hasItem(item)) {
+    carrinho.forEach(cartItem => {
+      if (cartItem.idProduto == item.idProduto) {
+        console.log('iguais')
+      }
+    })
+  }
+}
+
+for (var i = 0; i < btnAdicionar.length; i++) {
+  btnAdicionar[i].addEventListener('click', e => {
+    let key = e.target.getAttribute('key')
+
+    let id = produtos[key].id
+    let descricao = produtos[key].descricao
+    let tamanho = produtos[key].tamanho
+    let preco = produtos[key].tamanho
+    let quantidade = produtos[key].quantidade
+
+    const produto = {
+      idProduto: id,
+      descricaoProduto: descricao,
+      tamanhoProduto: tamanho,
+      precoProduto: preco,
+      quantidadeProduto: quantidade
+    }
+
+    salvarProdutoCarrinho(produto)
+  })
+}
